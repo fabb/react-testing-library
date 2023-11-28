4a3ec0a6082deade96e9eb3e00a1144f935d97f5
fabb <153960+fabb@users.noreply.github.com>
2023-11-28 17:01:15 +0000

    Update 5 files

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Form } from "./form";
+import { Form } from "./form.tsx";
 import "./index.css";
 
 const App = () => (
