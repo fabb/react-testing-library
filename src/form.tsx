21ae9658fdbc34bb5b593a6d68673e9085a5fe2c
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2023-11-29 11:57:21 +0100

    add type checking to build script

--- src/form.tsx
+++ src/form.tsx
@@ -1,39 +1,34 @@
-import React, { FunctionComponent } from "react";
-import "./form.css";
+import React, { FunctionComponent } from 'react'
+import './form.css'
 
 interface FormProps {
-  onSubmit: () => void;
+    onSubmit: () => void
 }
 
 export const Form: FunctionComponent<FormProps> = (props) => {
-  return (
-    <form
-      method="post"
-      noValidate={true}
-      onSubmit={(e) => {
-        e.stopPropagation();
-        e.preventDefault();
-        const formData = new FormData(e.currentTarget);
-        props.onSubmit(formData);
-      }}
-      data-testid="myForm"
-    >
-      <div>
-        <label htmlFor="my-input">Favorite color: </label>
-        <input type="text" id="my-input" name="my-input" />
-      </div>
+    return (
+        <form
+            method="post"
+            noValidate={true}
+            onSubmit={(e) => {
+                e.stopPropagation()
+                e.preventDefault()
+                const formData = new FormData(e.currentTarget)
+                props.onSubmit(formData)
+            }}
+            data-testid="myForm"
+        >
+            <div>
+                <label htmlFor="my-input">Favorite color: </label>
+                <input type="text" id="my-input" name="my-input" />
+            </div>
 
-      <div>
-        <label htmlFor="ok">Please check this checkbox</label>
-        <input
-          type="checkbox"
-          name="my-checkbox"
-          aria-label="my-checkbox"
-          id="ok"
-        />
-      </div>
+            <div>
+                <label htmlFor="ok">Please check this checkbox</label>
+                <input type="checkbox" name="my-checkbox" aria-label="my-checkbox" id="ok" />
+            </div>
 
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
+            <button type="submit">Submit</button>
+        </form>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,15 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Form } from "./form.tsx";
-import "./index.css";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Form } from './form'
+import './index.css'
 
 const App = () => (
-  <div>
-    <div>1. fix the compile error</div>
-    <div>2. fix the failing test</div>
-    <Form
-      onSubmit={(formData) =>
-        console.log(
-          "form was submitted with FormData",
-          Object.fromEntries(formData.entries()),
-        )
-      }
-    />
-  </div>
-);
+    <div>
+        <div>1. fix the compile error</div>
+        <div>2. fix the failing test</div>
+        <Form onSubmit={(formData) => console.log('form was submitted with FormData', Object.fromEntries(formData.entries()))} />
+    </div>
+)
 
-const root = createRoot(document.getElementById("root")!);
-root.render(<App />);
+const root = createRoot(document.getElementById('root')!)
+root.render(<App />)
